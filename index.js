baecbb88ff5e9c2fff8a4be90b29d454131eb0fd
Rubanraj R (Digital) <RU353746@wipro.com>
2018-03-01 17:21:40 +0530

    express server added

--- index.js
+++ index.js
@@ -3,16 +3,27 @@ const express = require('express')
     , { DialogflowApp } = require('actions-on-google')
     , dfApp = new DialogflowApp({request:request, response:response})
     , WelcomeIndent = 'input.welcome'
-    , OpportunityEmailID = 'input.oppEmailId';
+    , OpportunityEmailID = 'input.oppEmailId'
+    , PORT = process.env.PORT || 4141;
 
-function welcome(app){
-  app.ask('I am glad that you are interested in creating opportunity! Are you sure you want to proceed with creating opportunity');
-}
+app.post('/', (req, res) => {
+  function welcome(app){
+    app.ask('I am glad that you are interested in creating opportunity! Are you sure you want to proceed with creating opportunity');
+  }
 
-function oppEmailId(app){
-  app.tell('your email id is captured');
-}
+  function oppEmailId(app){
+    app.tell('your email id is captured');
+  }
 
-const actionMap = new Map();
-actionMap.set(WelcomeIndent, welcome);
-actionMap.set(OpportunityEmailID, oppEmailId);
+  const actionMap = new Map();
+  actionMap.set(WelcomeIndent, welcome);
+  actionMap.set(OpportunityEmailID, oppEmailId);
+});
+
+app.listen(PORT, (err, res) => {
+  if (err) {
+    console.log('err in starting server - > ', err);
+  } else {
+    console.log('server started in ', PORT);
+  }
+});
