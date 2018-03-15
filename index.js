435a0cfb7cdb827afb9a9a0ee0511e0a0a781cd4
rapidprototypes <36910655+rapidprototypes@users.noreply.github.com>
2018-03-15 12:25:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,36 +7,36 @@ const express = require('express')
     // , OpportunityEmailID = 'input.oppEmailId'
     , PORT = process.env.PORT || 4141;
 
-app.post('/ping', function(request, response){
-    console.log('req -- >'. request);
-  console.log('body -- > ', request.body);
-//   var dfApp = new DialogflowApp({request:req, response:res});
-//   function welcome(app){
-//     dfApp.ask('I am glad that you are interested in creating opportunity! Are you sure you want to proceed with creating opportunity');
-//   }
-//
-//   function oppEmailId(app){
-//     dfApp.tell('your email id is captured');
-//   }
-//
-//   const actionMap = new Map();
-//   actionMap.set(WelcomeIndent, welcome);
-//   actionMap.set(OpportunityEmailID, oppEmailId);
+// app.post('/ping', function(request, response){
+//     console.log('req -- >'. request);
+//   console.log('body -- > ', request.body);
+// //   var dfApp = new DialogflowApp({request:req, response:res});
+// //   function welcome(app){
+// //     dfApp.ask('I am glad that you are interested in creating opportunity! Are you sure you want to proceed with creating opportunity');
+// //   }
+// //
+// //   function oppEmailId(app){
+// //     dfApp.tell('your email id is captured');
+// //   }
+// //
+// //   const actionMap = new Map();
+// //   actionMap.set(WelcomeIndent, welcome);
+// //   actionMap.set(OpportunityEmailID, oppEmailId);
+//     var speech = 'from webhook';
+//     response.json({
+//         speech : speech,
+//         displayText : speech
+//     });
+// });
+
+app.post('/tom', function(request, response){
+    console.log('body -> ', request.body);
+    console.log('actions - > ', request.body.result.action);
     var speech = 'from webhook';
     response.json({
         speech : speech,
         displayText : speech
     });
-});
-
-app.post('/pong', function(request, response){
-    console.log('request -> ', request)
-    var speech = 'from webhook';
-    resp = {
-        speech : speech,
-        displayText : speech
-    }
-    response.send(resp)
 })
 
 server.listen(PORT, (err, res) => {
